d72bcabd6a369a265c7088d80d702f5ef58b1076
May <May@Summer.Afternoon>
2024-01-15 16:44:47 +0800

    得到动画与图层节点

--- Player.cs
+++ Player.cs
@@ -9,11 +9,13 @@ public partial class Player : CharacterBody2D
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public Sprite2D Sprite;
+	public Sprite2D PlayerSprite;
+	public AnimationPlayer PlayerAnimation;
 
     public override void _Ready()
     {
-		Sprite = GetNode<Sprite2D>("Sprite2D");
+		PlayerSprite = GetNode<Sprite2D>("Sprite2D");
+		PlayerAnimation = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
     public override void _PhysicsProcess(double delta)
@@ -23,14 +25,14 @@ public partial class Player : CharacterBody2D
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		PlayerAnimation.Play("idle");
+        // Handle Jump.
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
